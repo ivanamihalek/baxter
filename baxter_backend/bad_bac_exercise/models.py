84ec493413d2d39c0a8244b0850dd709e2bde778
Ivana Mihalek <ivana.mihalek@gmail.com>
2024-11-20 21:56:04 +0100

    models

--- baxter_backend/bad_bac_exercise/models.py
+++ baxter_backend/bad_bac_exercise/models.py
@@ -1,4 +1,29 @@
 from django.db import models
+"""
+MAIN TABLES
+    ucsc_assemblies
+    antibio_res_mutations
+    genes
+    drugs
+    drug_classes
+    pdb_structures
+    
+AUX TABLES
+    card_model_descriptions
+    taxonomy_names
+    
+JUNCTION TABLES
+    mutation2drug
+    mutation2drug_class
+    mutation2gene
+    gene2assembly
+    structure2gene
+    structure2drug
+    structure2drug_class
+"""
+
+#######################################################################
+#  MAIN TABLES
 
 
 class UCSCAssembly(models.Model):
@@ -12,3 +37,62 @@ class UCSCAssembly(models.Model):
 
     class Meta:
         db_table = 'ucsc_assemblies'
+
+
+class AntibioResMutation(models.Model):
+
+    class Meta:
+        db_table = 'ucsc_assemblies'
+        db_table_comment = 'Mutations conferring antibiotic resistance'
+
+
+class Gene(models.Model):
+
+    class Meta:
+        db_table = 'genes'
+
+
+class Drug(models.Model):
+
+    class Meta:
+        db_table = 'drugs'
+
+
+class DrugClass(models.Model):
+
+    class Meta:
+        db_table = 'drug_classes'
+
+
+class PDBStructure(models.Model):
+
+    class Meta:
+        db_table = 'pdb_structures'
+
+
+#######################################################################
+#  AUX TABLES
+class CARDModelDescription(models.Model):
+
+    class Meta:
+        db_table = 'card_model_description'
+        db_table_comment = 'CARD refers to Comprehensive Antibiotic Resistance Database'
+
+
+class TaxonomyName(models.Model):
+
+    class Meta:
+        db_table = 'taxonomy_names'
+
+#######################################################################
+# JUNCTION TABLES
+# https://zerotobyte.com/django-many-to-many-relationship-explained/
+# many to many with additional fields
+# https://stackoverflow.com/questions/4443190/djangos-manytomany-relationship-with-additional-fields
+#     mutation2drug
+#     mutation2drug_class
+#     mutation2gene
+#     gene2assembly
+#     structure2gene
+#     structure2drug
+#     structure2drug_class
--- baxter_backend/bad_bac_exercise/scripts/bb03_parse_card_json.py
+++ baxter_backend/bad_bac_exercise/scripts/bb03_parse_card_json.py
@@ -10,10 +10,14 @@ from pprint import pprint
 # reconstructing this, for example:
 # https://card.mcmaster.ca/ontology/40192
 
-# start building the database
-# is the reference sequence available in genome browser
-# https://hgdownload.soe.ucsc.edu/hubs/bacteria/index.html
-# any of those usable? is there any way to get to this data except page scraping?
+# store the card info, including the protein and cdna seqs
+
+# to map to available assemblies, download all uccs sdna seqs from ncbi, for ex
+# https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/012/525/GCF_000012525.1_ASM1252v1/
+# create searchable db (blat format)
+# then find mapping to assembly by blasting
+
+# any of ucsc assemblies usable? is there any way to get to this data except page scraping?
 # https://genome.ucsc.edu/cgi-bin/hgGateway paste in the assembly
 # if there aren't too many, can request here https://genome.ucsc.edu/assemblyRequest.html
 
@@ -25,39 +29,52 @@ from pprint import pprint
 # download structures and check proximity of the residues to the small molecule
 
 def parse_card_entry(card_entry: dict):
-    # pprint(card_entry)
+
+    card_description = card_entry["ARO_description"]
     print(card_entry["ARO_name"])
-    print(card_entry["ARO_description"])
+    print(card_description)
+    sequences = []
     for identifier, sub_entry in card_entry["model_sequences"]["sequence"].items():
         print()
         print("sequence entry:", identifier)
         pprint(sub_entry)
     print()
+    drugs = []
+    drug_classes = []
     for identifier, sub_entry in card_entry["ARO_category"].items():
         if "category_aro_class_name" not in sub_entry: continue
         if sub_entry["category_aro_class_name"] not in ["Drug Class", "Antibiotic"]: continue
         print()
         print("drug entry:", identifier)
         print("class name:", sub_entry["category_aro_class_name"])
-        print("drug name:", sub_entry["category_aro_name"])  # this may belong to an additional ddrug class
+        print("drug or class  name:", sub_entry["category_aro_name"])  # this may belong to an additional ddrug class
+        if sub_entry["category_aro_class_name"] == "Drug Class":
+            drug_classes.append(sub_entry["category_aro_name"])
+        else:
+            drugs.append(sub_entry["category_aro_name"])
 
     print("*************************\n")
+    return card_description, sequences, drugs, drug_classes
 
+def process_card_json(card_home: str, card_short2accession_number: dict):
 
-def process_card_json(card_home: str, accession_numbers: dict):
+    # it helps to keep in mind that 'card' here stands for the 'CARD' database name
+    # The Comprehensive Antibiotic Resistance Database
     jsonfile = f"{card_home}/card.json"
     card_dict = json.load(open(jsonfile))
 
-    acc_number2card_short = {v: k for k, v in accession_numbers.items()}
+    acc_number2card_short = {v: k for k, v in card_short2accession_number.items()}
 
     for card_id, card_entry in card_dict.items():
         if not isinstance(card_entry, dict): continue  # card_dict['_version'] == '3.3.0'
         if 'ARO_accession' not in card_entry: continue  # card_dict['description']
 
         if card_entry['ARO_accession'] not in acc_number2card_short.keys(): continue
-        print(card_id)
+        accession_number = card_entry['ARO_accession']
+        card_short_name = acc_number2card_short[accession_number]
+        print(card_id, card_short_name, accession_number)
         parse_card_entry(card_entry)
-
+        exit()
 
 def process_card_snps(card_home: str) -> tuple[dict, dict]:
     snp_file = f"{card_home}/snps.txt"
@@ -65,38 +82,38 @@ def process_card_snps(card_home: str) -> tuple[dict, dict]:
     ct = 0
 
     mutations = {}
-    accession_number = {}
+    card_short2accession_number = {}
     for line in inf:
         fields = line.strip().split("\t")
         if fields[0] == 'Accession': continue  # this is header
-        if fields[3] != 'single resistance variant': continue  # this is header
-        if fields[2] != 'protein variant model': continue  # this is header
+        if fields[3] != 'single resistance variant': continue
+        if fields[2] != 'protein variant model': continue
         if len(fields[1]) < 5: continue  # not sure what these are
         card_short_name = fields[-1]
 
         if card_short_name not in mutations: mutations[card_short_name] = []
         mutations[card_short_name].append(fields[-2])
 
-        if card_short_name in accession_number:
-            if accession_number[card_short_name] != fields[0]:
+        if card_short_name in card_short2accession_number:
+            if card_short2accession_number[card_short_name] != fields[0]:
                 print(f"unexpected multiple accession numbers for a short CARD name:")
-                print(f"{card_short_name}:  {accession_number[card_short_name]}  {fields[0]}")
+                print(f"{card_short_name}:  {card_short2accession_number[card_short_name]}  {fields[0]}")
         else:
-            accession_number[card_short_name] = fields[0]
+            card_short2accession_number[card_short_name] = fields[0]
         ct += 1
 
     # print(f"total protein mutations: {ct}")
     # print(f"total card entries: {len(mutations)}")
     # for card_short_name, mutation_list in mutations.items():
     #     print(f"{accession_numbers[card_short_name]} {card_short_name}  {mutation_list}")
-    return accession_number, mutations
+    return card_short2accession_number, mutations
 
 
 def run():
     card_home = "/storage/databases/CARD-data"
-    (accession_numbers, mutations) = process_card_snps(card_home)
+    (card_short2accession_number, mutations) = process_card_snps(card_home)
 
-    process_card_json(card_home, accession_numbers)
+    process_card_json(card_home, card_short2accession_number)
 
 
 #######################
