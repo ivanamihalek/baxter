8e5ceb3892641fbfc6f4a7d3712912c24cb31877
Ivana Mihalek <ivana.mihalek@gmail.com>
2024-11-20 13:01:03 +0100

    table of USSC bacterial genomes

--- baxter_backend/bad_bac_exercise/models.py
+++ baxter_backend/bad_bac_exercise/models.py
@@ -1,22 +1,14 @@
 from django.db import models
 
-# Create your models here.
-#
-# class Variant(models.Model):
-#     gdna_start             = models.IntegerField(blank=True, null=True)
-#     gdna_end               = models.IntegerField(blank=True, null=True)
-#     mod_type               = models.CharField(max_length=6, blank=True, null=True)
-#     nt_ref                 = models.CharField(max_length=50, blank=False, null=False)
-#     nt_alt                 = models.CharField(max_length=50, blank=False, null=False)
-#     cdna                   = models.CharField(max_length=100, blank=False, null=False)
-#     protein                = models.CharField(max_length=100, blank=True, null=True)
-#     splicing               = models.CharField(max_length=100, blank=True, null=True)
-#     gnomad_freqs_id        = models.ForeignKey(GnomadFreqs, on_delete=models.SET_NULL, null=True)
-#     protein_domain         = models.CharField(max_length=6, blank=True, null=True)
-#     sec_structure_element  = models.CharField(max_length=6, blank=True, null=True)
-#     present_in_ortho_verts = models.IntegerField(blank=True, null=True)
-#     present_in_para_verts  = models.IntegerField(blank=True, null=True)
-#     systems_effect         = models.CharField(max_length=9, blank=True, null=True)
-#
-#     class Meta:
-#         db_table = 'variants'
+
+class UCSCAssembly(models.Model):
+    ncbi_accession_number = models.CharField(max_length=50, blank=False, null=False, unique=True)
+    refseq_assembly_id   = models.CharField(max_length=50, blank=False, null=False)
+    common_name          = models.CharField(max_length=200, blank=False, null=False)
+    scientific_name      = models.CharField(max_length=100, blank=False, null=False)
+    biosample            = models.CharField(max_length=50, blank=False, null=False)
+    bioproject           = models.CharField(max_length=50, blank=False, null=False)
+    assembly_date        = models.DateField()
+
+    class Meta:
+        db_table = 'ucsc_assemblies'
