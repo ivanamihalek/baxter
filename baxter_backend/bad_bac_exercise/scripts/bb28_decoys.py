fca2e57ff618988da55cee5e806c1b33ff70043f
Ivana Mihalek <ivana.mihalek@gmail.com>
2024-12-03 04:58:54 +0100

    fudging towards the end

--- baxter_backend/bad_bac_exercise/scripts/bb28_decoys.py
+++ baxter_backend/bad_bac_exercise/scripts/bb28_decoys.py
@@ -2,31 +2,43 @@
 # this is meant to be run with
 # ./manage.py runscript bb03_parse_card_json
 # in that case django will take care of the paths and also check for migrations and such
-
+import os
 import random
+from glob import glob
 from os import listdir
 
 from Bio import Entrez
 
-from bad_bac_exercise.models import AntibioticResMutation, Pdb2Gene, Pdb2Drug, Pdb2Mutation, PDBStructure, Decoy
+from bad_bac_exercise.models import Decoy
+
+
+def select_from_single_seq_fasta(filepath):
+
+    with open(filepath) as inf:
+        seq = "".join(inf.read().replace(" ", "").split("\n")[1:])
+    if not seq:
+        return "Error: something went wrong"
+    if len(seq) < 5000:
+        return "Error: seq too short"
+    sample_length = random.choice(range(800, 1800))
+    sample_start  = random.choice(range(1, len(seq)-1800)) - 1
+    if sample_start < 0:
+        return "Error: seq too short"
+    sample = seq[sample_start:sample_start+sample_length]
+    if "N" in sample:
+        return "Error: sequencing error present"
+    print(sample_start, sample_length)
+    print(sample)
+    return sample
 
 
 def create_phage_decoy(seqs_dir):
     random_fasta_file = random.choice(list(listdir(seqs_dir)))
     print(random_fasta_file)
-    with open(f"{seqs_dir}/{random_fasta_file}") as inf:
-       seq = "".join(inf.read().replace(" ", "").split("\n")[1:])
+    sample =  select_from_single_seq_fasta(f"{seqs_dir}/{random_fasta_file}")
+    if sample[:3] == "Err": return sample
 
-    sample_length = random.choice(range(800, 1800))
-    sample_start  = random.choice(range(1, len(seq)-1800)) - 1
-    if sample_start < 0:
-        return "seq too short"
-    sample = seq[sample_start:sample_start+sample_length]
-    if "N" in sample:
-        return "sequencing error present"
     # otherwise store
-    print(sample_start, sample_length)
-    print(sample)
     decoy_table_entry = {
         "source": Decoy.Source.translate("phages"),
         "identifier": random_fasta_file.replace(".fasta", ''),
@@ -37,6 +49,25 @@ def create_phage_decoy(seqs_dir):
     return "ok"
 
 
+def create_human_microbiome_decoy(seqs_dir):
+    i_came_from = os.getcwd()
+    random_species =  random.choice(list(listdir(seqs_dir)))
+    os.chdir(f"{seqs_dir}/{random_species}")
+    print(random_species)
+    random_fasta_file = random.choice(list(glob("*.fna")))
+    print(random_fasta_file)
+    sample = select_from_single_seq_fasta(random_fasta_file)
+    if sample[:3] == "Err": return sample
+    # otherwise store
+    decoy_table_entry = {
+        "source": Decoy.Source.translate("human_microbiome"),
+        "identifier": random_fasta_file.replace(".fna", ''),
+        "dna_seq": sample,
+    }
+    Decoy(**decoy_table_entry).save()
+    os.chdir(i_came_from)
+
+
 def run():
     # https://phagescope.deepomics.org/download#fasta
     phagi_fasta_dir  = "/storage/databases/phagescope/RefSeq"  # one sequence per data file
@@ -44,5 +75,7 @@ def run():
     human_microbiome = "/storage/databases/ncbi/human_microbiome"
     # https://ftp.ncbi.nlm.nih.gov/genomes/genbank/plant
     plant_genomes    = "/storage/databases/ncbi/plant_genomes"
-    for _ in range(10):
-        create_phage_decoy(phagi_fasta_dir)
+    # for _ in range(10):
+    #     create_phage_decoy(phagi_fasta_dir)
+    # for _ in range(10):
+    #     create_human_microbiome_decoy(human_microbiome)
